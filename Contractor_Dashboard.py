459ea8ee84f1626974478ce581c832ff53071145
ibrahimelnagar <ebraheem.elnagar@gmail.com>
2024-12-18 01:14:50 +0200

    Update Contractor_Dashboard.py

--- Contractor_Dashboard.py
+++ Contractor_Dashboard.py
@@ -9,11 +9,32 @@ with col2:
 
 # Embed the Power BI dashboard using the provided HTML embed code
 power_bi_html = """
-<div>
-    <iframe title="Report Section" width="100%" height="800" src="https://app.powerbi.com/reportEmbed?reportId=91071687-dcbb-433a-a3a3-159775676fce&autoAuth=true&ctid=a86ca211-c918-4c77-8b32-440c27aa3100" frameborder="0" allowFullScreen="true"></iframe>
+<div style="position: relative; width: 100%; height: 0; padding-bottom: 75%;">
+    <iframe title="Report Section" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;"
+            src="https://app.powerbi.com/reportEmbed?reportId=91071687-dcbb-433a-a3a3-159775676fce&autoAuth=true&ctid=a86ca211-c918-4c77-8b32-440c27aa3100"
+            frameborder="0" allowFullScreen="true"></iframe>
 </div>
 """
 st.markdown(power_bi_html, unsafe_allow_html=True)
+
+# Add a full-screen button
+st.markdown("""
+    <button onclick="openFullscreen();">Full Screen</button>
+    <script>
+    function openFullscreen() {
+      var elem = document.querySelector('iframe');
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.mozRequestFullScreen) { // Firefox
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { // IE/Edge
+        elem.msRequestFullscreen();
+      }
+    }
+    </script>
+    """, unsafe_allow_html=True)
 
 # Add credit for the dashboard creator
 st.markdown("""
@@ -27,13 +48,4 @@ st.markdown(
     """
     <style>
     .main .block-container{{
-        max-width: 100%;
-        padding-top: 2rem;
-        padding-right: 2rem;
-        padding-left: 2rem;
-        padding-bottom: 2rem;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+        max-width: 1
