f2671ce0dfc8a5306cc03e6cb836cddbacddc02c
ibrahimelnagar <ebraheem.elnagar@gmail.com>
2024-12-18 01:00:37 +0200

    Update Contractor_Dashboard.py

--- Contractor_Dashboard.py
+++ Contractor_Dashboard.py
@@ -3,9 +3,13 @@ import streamlit as st
 # Set the title of the web page with the logo beside it
 col1, col2 = st.columns([1, 10])
 with col1:
-    st.image("NATGAS.png", width=200)
+    st.image("NATGAS.png", width=100)
 with col2:
     st.title("NATGAS Contractors Monthly Dashboard")
+
+# Embed the Power BI dashboard with a placeholder URL
+power_bi_url = "https://www.example.com"  # Replace with your actual Power BI dashboard URL
+st.markdown(f'<iframe width="100%" height="800" src="{power_bi_url}" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
 
 # Add credit for the dashboard creator
 st.markdown("""
@@ -13,10 +17,6 @@ st.markdown("""
         <p><strong>Created by:</strong> Ibrahim Elnagar, Operation Manager</p>
     </div>
     """, unsafe_allow_html=True)
-
-# Embed the Power BI dashboard
-power_bi_url = "https://app.powerbi.com/view?r=your_power_bi_dashboard_url"
-st.markdown(f'<iframe width="100%" height="800" src="{power_bi_url}" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
 
 # Add some styling to make the dashboard take the majority of the screen
 st.markdown(
