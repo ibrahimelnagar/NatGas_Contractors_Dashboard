6117c2b31dc9899063fb0c17b4725ac607d3cc48
ibrahimelnagar <ebraheem.elnagar@gmail.com>
2024-12-18 01:01:26 +0200

    Update Contractor_Dashboard.py

--- Contractor_Dashboard.py
+++ Contractor_Dashboard.py
@@ -6,17 +6,18 @@ with col1:
     st.image("NATGAS.png", width=100)
 with col2:
     st.title("NATGAS Contractors Monthly Dashboard")
-
-# Embed the Power BI dashboard with a placeholder URL
-power_bi_url = "https://www.example.com"  # Replace with your actual Power BI dashboard URL
-st.markdown(f'<iframe width="100%" height="800" src="{power_bi_url}" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
-
 # Add credit for the dashboard creator
 st.markdown("""
     <div style='text-align: right;'>
         <p><strong>Created by:</strong> Ibrahim Elnagar, Operation Manager</p>
     </div>
     """, unsafe_allow_html=True)
+
+# Embed the Power BI dashboard with a placeholder URL
+power_bi_url = "https://www.example.com"  # Replace with your actual Power BI dashboard URL
+st.markdown(f'<iframe width="100%" height="800" src="{power_bi_url}" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
+
+
 
 # Add some styling to make the dashboard take the majority of the screen
 st.markdown(
