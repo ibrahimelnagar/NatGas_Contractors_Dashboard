8b3eb5e0b5bf69b1251f4261bdd123f53e8c65bb
ibrahimelnagar <ebraheem.elnagar@gmail.com>
2024-12-18 01:57:12 +0200

    Update Contractor_Dashboard.py

--- Contractor_Dashboard.py
+++ Contractor_Dashboard.py
@@ -1,16 +1,22 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Natgas Contractors Monthly Dash-board")
-st.image("NATGAS.png", width=120,)
+# Page configuration
+st.set_page_config(page_title="Natgas Contractors Monthly Dashboard")
 
-st.title("Natgas Contractors Monthly Dash-board")
-components.iframe("https://app.powerbi.com/reportEmbed?reportId=91071687-dcbb-433a-a3a3-159775676fce&autoAuth=true&ctid=a86ca211-c918-4c77-8b32-440c27aa3100", width=800, height=600)
+# Display logo
+st.image("NATGAS.png", width=120)
 
+# Page title
+st.title("Natgas Contractors Monthly Dashboard")
+
+# Add a clickable link to the Power BI dashboard
+st.write("[Click here to view the dashboard](https://app.powerbi.com/groups/me/reports/91071687-dcbb-433a-a3a3-159775676fce)")
+
+# Hide Streamlit's default styling
 hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
 st.markdown(hide_st_style, unsafe_allow_html=True)
