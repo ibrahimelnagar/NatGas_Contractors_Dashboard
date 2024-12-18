e353970e9e1e3540caed219b01a6a75c768ace47
ibrahimelnagar <ebraheem.elnagar@gmail.com>
2024-12-18 02:23:40 +0200

    Update Contractor_Dashboard.py

--- Contractor_Dashboard.py
+++ Contractor_Dashboard.py
@@ -1,22 +1,39 @@
 import streamlit as st
 
-# Page configuration
-st.set_page_config(page_title="Natgas Contractors Monthly Dashboard")
+# Set the title of the web page with the logo beside it
+col1, col2 = st.columns([1, 10])
+with col1:
+    st.image("NATGAS.PNG", width=100)
+with col2:
+    st.title("NATGAS Contractors Monthly Dashboard")
 
-# Display logo
-st.image("NATGAS.png", width=120)
-
-# Page title
-st.title("Natgas Contractors Monthly Dashboard")
-
-# Add a clickable link to the Power BI dashboard
-st.write("[Click here to view the dashboard](https://app.powerbi.com/reportEmbed?reportId=91071687-dcbb-433a-a3a3-159775676fce&autoAuth=true&ctid=a86ca211-c918-4c77-8b32-440c27aa3100)")
-
-# Hide Streamlit's default styling
-hide_st_style = """
-<style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-</style>
+# Embed the Power BI dashboard using the provided HTML embed code
+power_bi_html = """
+<div>
+    <iframe title="Report Section" width="100%" height="800" src="https://app.powerbi.com/reportEmbed?reportId=91071687-dcbb-433a-a3a3-159775676fce&autoAuth=true&ctid=a86ca211-c918-4c77-8b32-440c27aa3100" frameborder="0" allowFullScreen="true"></iframe>
+</div>
 """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(power_bi_html, unsafe_allow_html=True)
+
+# Add credit for the dashboard creator
+st.markdown("""
+    <div style='text-align: right;'>
+        <p><strong>Created by:</strong> Ibrahim Elnagar, Operation Manager</p>
+    </div>
+    """, unsafe_allow_html=True)
+
+# Add some styling to make the dashboard take the majority of the screen
+st.markdown(
+    """
+    <style>
+    .main .block-container{{
+        max-width: 100%;
+        padding-top: 2rem;
+        padding-right: 2rem;
+        padding-left: 2rem;
+        padding-bottom: 2rem;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
